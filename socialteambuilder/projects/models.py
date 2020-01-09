8516613427ed66a08b3b48b61fee614c70ffba6f
Mark Ryan <markryandeveloper@gmail.com>
2020-01-08 22:15:45 -0500

    added projects position model

--- socialteambuilder/projects/models.py
+++ socialteambuilder/projects/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from accounts.models import Skill
 
 User = get_user_model()
 
@@ -14,11 +15,41 @@ class Project(models.Model):
         ('B', 'Closed'),
         ('C', 'Complete'),
     )
-    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name="projects")
+    owner = models.ForeignKey(
+        User, 
+        on_delete=models.PROTECT, 
+        related_name="projects"
+    )
     title = models.CharField(max_length=255, unique=True)
     description = models.TextField(max_length=2000)
     time_estimate = models.IntegerField()
     applicant_requirements = models.TextField(max_length=2000)
     status = models.CharField(max_length=1, choices=STATUS)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
+class Position(models.Model):
+    """Model representing a single position within a developing project
+    that users can apply to
+    """
+    STATUS = (
+        ('E', 'Empty'),
+        ('F', 'Filled')
+    )
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000)
+    skills = models.ManyToManyField(Skill, related_name="positions")
+    status = models.CharField(max_length=1, choices=STATUS)
+    time_estimate = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+    
+
+# class Application(models.Model):
+#     pass
+    
