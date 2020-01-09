4d75c44eb1ccc0467b960db118e9f20a86888340
Mark Ryan <markryandeveloper@gmail.com>
2020-01-08 21:58:25 -0500

    added projects project model

--- socialteambuilder/projects/models.py
+++ socialteambuilder/projects/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Project(models.Model):
+    """Model representing a development project that users can create 
+    and/or join
+    """
+    STATUS = (
+        ('A', 'Open'),
+        ('B', 'Closed'),
+        ('C', 'Complete'),
+    )
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name="projects")
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField(max_length=2000)
+    time_estimate = models.IntegerField()
+    applicant_requirements = models.TextField(max_length=2000)
+    status = models.CharField(max_length=1, choices=STATUS)
+
+
