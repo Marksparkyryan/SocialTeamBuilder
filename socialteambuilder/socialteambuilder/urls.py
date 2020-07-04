763b2b11564619f854a786ada2a421e377063e5e
Mark Ryan <markryandeveloper@gmail.com>
2020-07-03 23:54:52 -0400

    fixking typo again

--- socialteambuilder/socialteambuilder/urls.py
+++ socialteambuilder/socialteambuilder/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('markdownx/', include(markdownx)),
 ]
 
-if settings.Debug:
+if settings.DEBUG:
     urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
