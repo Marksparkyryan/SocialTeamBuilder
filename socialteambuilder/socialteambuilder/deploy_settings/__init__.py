cfc40c8e74de12be03c80511c1edb3adfe4e9093
Mark Ryan <markryandeveloper@gmail.com>
2020-07-03 23:43:07 -0400

    modifying main url patterns and appending of media url

--- socialteambuilder/socialteambuilder/deploy_settings/__init__.py
+++ socialteambuilder/socialteambuilder/deploy_settings/__init__.py
@@ -30,7 +30,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//{}.s3.amazonaws.com/media/'.format(AWS_STORAGE_BUCKET_NAME)
 MEDIA_ROOT = MEDIA_URL
 
 db_from_env = dj_database_url.config()
--- socialteambuilder/socialteambuilder/urls.py
+++ socialteambuilder/socialteambuilder/urls.py
@@ -19,6 +19,9 @@ urlpatterns = [
     path('select2/', include('django_select2.urls')),
     # path('__debug__/', include(debug_toolbar.urls)),
     path('markdownx/', include(markdownx)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if Debug:
+    urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
