57345f3b859781308d461a83ab9ac1bb180883e1
Mark Ryan <markryandeveloper@gmail.com>
2020-07-03 22:28:01 -0400

    added S3 settings config

--- socialteambuilder/socialteambuilder/deploy_settings/__init__.py
+++ socialteambuilder/socialteambuilder/deploy_settings/__init__.py
@@ -21,6 +21,14 @@ ALLOWED_HOSTS = [
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 
+INSTALLED_APPS.append('storages')
+
+AWS_ACCESS_KEY_ID = get_env_variable("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY_ID = get_env_variable("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = get_env_variable("S3_BUCKET_NAME")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
